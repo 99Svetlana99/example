19de4bd35d7cc3c7e368cf3fdeac6e716df6befb
Svetlana Tantsura <tantsura_s99@mail.ru>
2022-08-20 21:59:38 +0300

    Lesson3

---
+++ Lesson3/example/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Написать метод для разворота массива, не используя память под дополнительный массив
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+string result = "";
+for (int i = 0; i < (array.Length / 2); i++)
+{
+    int buff = array[i];
+    array[i] = array[array.Length - i - 1];
+    array[array.Length - i - 1] = buff;
+}
+for (int i = 0; i < (array.Length); i++)
+{
+    result += (array[i] + " ");
+}
+Console.WriteLine($"Перевернутый массив - {result}");
---
+++ Lesson3/example019/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = number;
+if (number >= 10000 & number <= 99999)
+{
+    int digit = number % 10;
+    number = number / 10;
+    int number1 = digit * 10000;
+    digit = number % 10;
+    number = number / 10;
+    number1 += digit * 1000;
+    digit = number % 10;
+    number = number / 10;
+    number1 += digit * 100;
+    digit = number % 10;
+    number = number / 10;
+    number1 += digit * 10;
+    digit = number % 10;
+    number = number / 10;
+    number1 += digit;
+
+    if (startNumber == number1)
+    {
+        Console.WriteLine($"{number1} является палиндромом ");
+    }
+    else
+    {
+        Console.WriteLine($"{number1}  не является палиндромом ");
+    }
+}
+else
+{
+    Console.WriteLine("Введите пятизначное число");
+}
+
+
+
